57fc4973da390c6c34d042fe5102b43fada6fa36
Andy <aejefferson@icloud.com>
2025-04-29 23:48:57 +0200

    add lllms module export for vibe-rules

--- alchemy/src/llms/index.ts
+++ alchemy/src/llms/index.ts
@@ -1,3 +1,20 @@
-import llms from "./llms.txt";
+import type { PackageRuleItem } from 'vibe-rules';
 
-export default llms;
+import { gettingStarted, whatIsAlchemy } from './rules.js';
+
+const rules: PackageRuleItem[] = [
+    {
+      name: 'alchemy-getting-started',
+      description: 'Instructions for installing and configuring Alchemy',
+      rule: gettingStarted,
+      alwaysApply: false // We have to instruct users to @ this rule when setting up alchemy initially
+    },
+    {
+      name: 'alchemy-what-is-alchemy',
+      description: 'Instructions for using Alchemy IaC',
+      rule: whatIsAlchemy,
+      alwaysApply: true // This will always be included in user's cursor rules
+    }
+];
+
+export default rules;
