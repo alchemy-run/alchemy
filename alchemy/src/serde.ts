db4d4f162da6acb09b5c44de45cdda33bd42ccc9
Sam Goodwin <sam@alchemy.run>
2025-05-23 15:06:55 -0700

    fix broken link

--- alchemy/src/serde.ts
+++ alchemy/src/serde.ts
@@ -115,7 +115,7 @@ export async function serialize(
     if (!scope.password) {
       throw new Error(
         "Cannot serialize secret without password, did you forget to set password when initializing your alchemy app?\n" +
-          "See: https://alchemy.dev/docs/getting-started/setup#password",
+          "See: https://alchemy.run/docs/concepts/secret.html#encryption-password",
       );
     }
     return {
@@ -207,7 +207,7 @@ export async function deserialize(
       if (!scope.password) {
         throw new Error(
           "Cannot deserialize secret without password, did you forget to set password when initializing your alchemy app?\n" +
-            "See: https://alchemy.dev/docs/getting-started/setup#password",
+            "See: https://alchemy.run/docs/concepts/secret.html#encryption-password",
         );
       }
       return new Secret(await decryptWithKey(value["@secret"], scope.password));
