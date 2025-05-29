a4ec3a0bd955b88df154af2c890b72c82b520b17
Sam Goodwin <sam@alchemy.run>
2025-05-29 04:55:14 -0700

    update vite.js to use the output of vite build

--- alchemy/src/cloudflare/vite.ts
+++ alchemy/src/cloudflare/vite.ts
@@ -18,6 +18,23 @@ export async function Vite<B extends Bindings>(
   const defaultAssets = path.join("dist", "client");
   return Website(id, {
     ...props,
+    // Alchemy should bundle the result of `vite built`, not the user's main
+    // TODO: we probably need bundling to properly handle WASM/rules
+    main: path.join(props.cwd ?? process.cwd(), "build", "server", "index.js"),
+    wrangler: {
+      // wrangler should point to the user's main (e.g. `worker.ts`), unless overridden
+      main:
+        typeof props.wrangler === "object"
+          ? (props.wrangler.main ?? props.main)
+          : props.main,
+      // write to wrangler.json by default but respect overrides
+      path:
+        typeof props.wrangler === "string"
+          ? props.wrangler
+          : typeof props.wrangler === "object"
+            ? props.wrangler.path
+            : undefined,
+    },
     assets:
       typeof props.assets === "object"
         ? {
