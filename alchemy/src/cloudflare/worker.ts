c676a359327f29d06c9e6ec32c5820d6b75ebf19
Sam Goodwin <sam@alchemy.run>
2025-06-20 00:39:24 -0700

    remove the unnecessary putWorker

--- alchemy/src/cloudflare/worker.ts
+++ alchemy/src/cloudflare/worker.ts
@@ -1054,23 +1054,6 @@ export const _Worker = Resource(
       // Delete any queue consumers attached to this worker first
       await deleteQueueConsumers(api, workerName);
 
-      // @ts-ignore
-      await uploadWorkerScript({
-        ...props,
-        entrypoint: undefined,
-        noBundle: false,
-        script:
-          props.format === "cjs"
-            ? "addEventListener('fetch', event => { event.respondWith(new Response('hello world')); });"
-            : "export default { fetch(request) { return new Response('hello world'); }, queue: () => {} }",
-        bindings: {} as B,
-        // we are writing a stub worker (to remove binding/event source dependencies)
-        // queue consumers will no longer exist by this point
-        eventSources: undefined,
-        // stub worker doesn't need dispatch namespace
-        namespace: undefined,
-      });
-
       await withExponentialBackoff(
         () =>
           deleteWorker(this, api, {
