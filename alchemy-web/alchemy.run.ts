b8ce5da82911df02fdf30d414be9210a0d8e6df9
Sam Goodwin <sam@alchemy.run>
2025-06-24 22:28:02 -0700

    publish website

--- alchemy-web/alchemy.run.ts
+++ alchemy-web/alchemy.run.ts
@@ -1,6 +1,5 @@
 import alchemy from "alchemy";
 import { DOStateStore, Website } from "alchemy/cloudflare";
-import { GitHubComment } from "alchemy/github";
 
 const app = await alchemy("alchemy:website", {
   stateStore: (scope) => new DOStateStore(scope),
@@ -14,14 +13,14 @@ const website = await Website("alchemy-website-test", {
   version: process.env.PULL_REQUEST,
 });
 
-if (process.env.PULL_REQUEST) {
-  await GitHubComment("alchemy-website-test", {
-    owner: "sam-goodwin",
-    repository: "alchemy",
-    issueNumber: Number(process.env.PULL_REQUEST),
-    body: `Website deployed to ${website.url}`,
-  });
-}
+// if (process.env.PULL_REQUEST) {
+//   await GitHubComment("alchemy-website-test", {
+//     owner: "sam-goodwin",
+//     repository: "alchemy",
+//     issueNumber: Number(process.env.PULL_REQUEST),
+//     body: `Website deployed to ${website.url}`,
+//   });
+// }
 
 console.log(website.url);
 
