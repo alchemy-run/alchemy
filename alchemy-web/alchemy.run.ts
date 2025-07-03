1583caeada1b55e22413dd7b96f9a320b7a3026c
Sam Goodwin <sam@alchemy.run>
2025-07-03 14:58:47 -0700

    set up pushing to pr domain

--- alchemy-web/alchemy.run.ts
+++ alchemy-web/alchemy.run.ts
@@ -2,16 +2,28 @@ import alchemy from "alchemy";
 import { DOStateStore, Website } from "alchemy/cloudflare";
 import { GitHubComment } from "alchemy/github";
 
+const stage = process.env.PULL_REQUEST ?? process.env.STAGE ?? "dev";
+
 const app = await alchemy("alchemy:website", {
   stateStore: (scope) => new DOStateStore(scope),
-  stage: process.env.PULL_REQUEST ?? "dev",
+  stage,
 });
 
-const website = await Website("alchemy-website-test", {
+const domain =
+  stage === "prod"
+    ? "alchemy.run"
+    : stage === "dev"
+      ? "dev.alchemy.run"
+      : `pr-${process.env.PULL_REQUEST}.alchemy.run`;
+
+const website = await Website("website", {
+  name: "alchemy-website",
   command: "bun run build",
   assets: "dist",
+  adopt: true,
   wrangler: false,
-  version: process.env.PULL_REQUEST,
+  version: stage === "prod" ? undefined : stage,
+  domains: process.env.PULL_REQUEST ? undefined : [domain],
 });
 
 if (process.env.PULL_REQUEST) {
@@ -33,6 +45,6 @@ This preview was built from commit ${process.env.GITHUB_SHA}
   });
 }
 
-console.log(website.url);
+console.log(domain ? `https://${domain}` : website.url);
 
 await app.finalize();
