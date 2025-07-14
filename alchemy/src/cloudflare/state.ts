a8b10b8174444a96df5ae76b6bbefa2968c73ad6
John Royal <34844819+john-royal@users.noreply.github.com>
2025-07-14 14:17:47 -0400

    chore: add back DOFSStateStore

--- alchemy/src/cloudflare/state.ts
+++ alchemy/src/cloudflare/state.ts
@@ -1,16 +1,20 @@
-import * as state from "../state/index.ts";
+import {
+  D1StateStore as _D1StateStore,
+  DOFSStateStore as _DOFSStateStore,
+  R2RestStateStore as _R2RestStateStore,
+} from "alchemy/state";
 
 /**
  * @deprecated Use `R2RestStateStore` from `alchemy/state` instead.
  */
-export const R2RestStateStore = state.R2RestStateStore;
+export const R2RestStateStore = _R2RestStateStore;
 
 /**
- * @deprecated Use `DOStateStore` from `alchemy/state` instead.
+ * @deprecated This state store is no longer recommended for production use. Please use {@link https://alchemy.run/guides/cloudflare-state-store/ CloudflareStateStore} from `alchemy/state` instead.
  */
-export const DOStateStore = state.CloudflareStateStore;
+export const DOStateStore = _DOFSStateStore;
 
 /**
  * @deprecated Use `D1StateStore` from `alchemy/state` instead.
  */
-export const D1StateStore = state.D1StateStore;
+export const D1StateStore = _D1StateStore;
