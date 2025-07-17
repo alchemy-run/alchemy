312f05f88f43184d43e150bd0de7b57a98ca3e80
John Royal <34844819+john-royal@users.noreply.github.com>
2025-07-17 00:22:46 -0400

    chore: init alchemy in livestore example

--- examples/cloudflare-livestore/vite.config.ts
+++ examples/cloudflare-livestore/vite.config.ts
@@ -1,5 +1,4 @@
 /* eslint-disable unicorn/no-process-exit */
-import { spawn } from "node:child_process";
 
 import { livestoreDevtoolsPlugin } from "@livestore/devtools-vite";
 import react from "@vitejs/plugin-react";
@@ -13,29 +12,5 @@ export default defineConfig({
   plugins: [
     react(),
     livestoreDevtoolsPlugin({ schemaPath: "./src/livestore/schema.ts" }),
-    // Running `wrangler dev` as part of `vite dev` needed for `@livestore/sync-cf`
-    {
-      name: "wrangler-dev",
-      configureServer: async (server) => {
-        const wrangler = spawn("wrangler", ["dev", "--port", "8787"], {
-          stdio: ["ignore", "inherit", "inherit"],
-        });
-
-        const shutdown = () => {
-          if (wrangler.killed === false) {
-            wrangler.kill();
-          }
-          process.exit(0);
-        };
-
-        server.httpServer?.on("close", shutdown);
-        process.on("SIGTERM", shutdown);
-        process.on("SIGINT", shutdown);
-
-        wrangler.on("exit", (code) =>
-          console.error(`wrangler dev exited with code ${code}`),
-        );
-      },
-    },
   ],
 });
