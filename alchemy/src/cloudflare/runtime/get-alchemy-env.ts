057494d828f4d85f92449dbc48f71f55150d31a4
John Royal <34844819+john-royal@users.noreply.github.com>
2025-07-31 18:50:48 -0400

    fix(astro): platform proxy

--- alchemy/src/cloudflare/runtime/get-alchemy-env.ts
+++ alchemy/src/cloudflare/runtime/get-alchemy-env.ts
@@ -1,14 +1,24 @@
+import type { GetPlatformProxyOptions } from "wrangler";
 import { resolveRuntimePaths } from "./resolve-runtime-paths.ts";
 
-export const getAlchemyEnv = async <E>() => {
-  const paths = resolveRuntimePaths();
+export const getAlchemyEnv = async <E>(
+  options: GetPlatformProxyOptions = {},
+) => {
   const { getPlatformProxy } = await import("wrangler");
-  const proxy = await getPlatformProxy({
+  const proxy = await getPlatformProxy(getPlatformProxyOptions(options));
+  return proxy.env as E;
+};
+
+export const getPlatformProxyOptions = (
+  options: GetPlatformProxyOptions = {},
+) => {
+  const paths = resolveRuntimePaths();
+  return {
     configPath: paths.config,
     persist: { path: paths.persist },
     experimental: {
       remoteBindings: true,
     },
-  });
-  return proxy.env as E;
+    ...options,
+  } satisfies GetPlatformProxyOptions;
 };
--- examples/cloudflare-astro/astro.config.mjs
+++ examples/cloudflare-astro/astro.config.mjs
@@ -1,8 +1,11 @@
 import cloudflare from '@astrojs/cloudflare';
+import { getPlatformProxyOptions } from 'alchemy/cloudflare/runtime';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: getPlatformProxyOptions(),
+  }),
 });
