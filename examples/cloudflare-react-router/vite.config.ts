4714cc57426b7ab73868766dd5b1932e0c7efc96
John Royal <34844819+john-royal@users.noreply.github.com>
2025-08-03 16:22:36 -0400

    update react router plugin name

--- examples/cloudflare-react-router/vite.config.ts
+++ examples/cloudflare-react-router/vite.config.ts
@@ -1,14 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import alchemyReactRouter from "alchemy/cloudflare/react-router";
+import alchemyCloudflare from "alchemy/cloudflare/react-router";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    alchemyReactRouter(),
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [alchemyCloudflare(), tailwindcss(), reactRouter(), tsconfigPaths()],
 });
