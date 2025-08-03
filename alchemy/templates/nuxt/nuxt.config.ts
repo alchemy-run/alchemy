feb82f3bd9deaf3727888ff7b9d9ad9f8e70b9fc
John Royal <34844819+john-royal@users.noreply.github.com>
2025-08-03 18:49:12 -0400

    remove compatibility dates

--- alchemy/templates/nuxt/nuxt.config.ts
+++ alchemy/templates/nuxt/nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-26",
   devtools: { enabled: true },
   nitro: {
     preset: "cloudflare_module",
--- examples/cloudflare-nuxt-pipeline/nuxt.config.ts
+++ examples/cloudflare-nuxt-pipeline/nuxt.config.ts
@@ -4,7 +4,6 @@ import alchemy from "alchemy/cloudflare/nuxt";
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-04-21",
   devtools: { enabled: true },
   nitro: {
     preset: "cloudflare-module",
