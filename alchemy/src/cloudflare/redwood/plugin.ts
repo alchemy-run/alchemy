5abf2cf5a4f86210761bb1c38e79db02965c0be9
John Royal <34844819+john-royal@users.noreply.github.com>
2025-08-04 16:44:55 -0400

    fix(redwood): exclude cf plugin

--- alchemy/src/cloudflare/redwood/plugin.ts
+++ alchemy/src/cloudflare/redwood/plugin.ts
@@ -8,6 +8,7 @@ const alchemyRedwood = (options?: RedwoodPluginOptions) => {
     redwood({
       ...options,
       configPath,
+      includeCloudflarePlugin: false,
     }),
     alchemyVite({
       configPath,
