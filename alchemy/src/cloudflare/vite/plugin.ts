6ffc46c06d7496ddff0c56084796edfa881ec1a4
John Royal <34844819+john-royal@users.noreply.github.com>
2025-08-19 16:02:13 -0400

    fix more missing types :(

--- alchemy/src/cloudflare/vite/plugin.ts
+++ alchemy/src/cloudflare/vite/plugin.ts
@@ -25,7 +25,7 @@ const alchemy = (config?: PluginConfig): PluginOption => {
     experimental: config?.experimental ?? {
       remoteBindings: true,
     },
-  });
+  }) as PluginOption;
 };
 
 export default alchemy;
