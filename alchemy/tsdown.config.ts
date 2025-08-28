2f29a253832d5c14e61401451a3734ef0c6d3bad
John Royal <34844819+john-royal@users.noreply.github.com>
2025-08-28 18:47:06 -0400

    chore: use tsdown for transpilation and include cjs build

--- alchemy/tsdown.config.ts
+++ alchemy/tsdown.config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from "tsdown";
+import pkg from "./package.json";
 
-export default defineConfig({
-  entry: ["bin/alchemy.ts"],
-  format: ["esm"],
-  clean: false,
-  shims: true,
-  outDir: "bin",
-  outputOptions: {
-    inlineDynamicImports: true,
-    banner: "#!/usr/bin/env node",
+const entry = Object.values(pkg.exports).map((e) => e.bun);
+
+export default defineConfig([
+  {
+    entry: ["bin/alchemy.ts"],
+    format: ["esm"],
+    clean: false,
+    shims: true,
+    outDir: "bin",
+    outputOptions: {
+      inlineDynamicImports: true,
+      banner: "#!/usr/bin/env node",
+    },
+    noExternal: ["execa", "open", "env-paths"],
   },
-  noExternal: ["execa", "open", "env-paths"]
-});
+  {
+    entry,
+    format: ["esm", "cjs"],
+    clean: true,
+    shims: true,
+    unbundle: true,
+    dts: true,
+    external: () => true,
+  },
+]);
