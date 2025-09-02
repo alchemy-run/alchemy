7500ba6e617d99ebf7193a2d0013a32d8dc2362d
Sam Goodwin <sam@alchemy.run>
2025-09-01 22:54:46 -0700

    define a Worker type and model Worker-to-Worker

--- alchemy/src/effect/index.ts
+++ alchemy/src/effect/index.ts
@@ -1,192 +1,245 @@
 import * as Effect from "effect/Effect";
 
-export interface Binding<Resource extends string, Action extends string> {
-  readonly resource: Resource;
+export interface Binding<ID extends string, Action extends string> {
+  readonly resource: ID;
   readonly action: Action;
 }
 
 // TODO(sam): are there errors?
 export type SendMessageError = never;
 
-export type Queue<Resource extends string, Msg> = {
+export type Queue<ID extends string, Msg> = {
   send(
     message: Msg,
-  ): Effect.Effect<void, SendMessageError, Queue.Send<Resource, Msg>>;
+  ): Effect.Effect<void, SendMessageError, Queue.Send<ID, Msg>>;
   sendBatch(
     ...message: Msg[]
-  ): Effect.Effect<void, SendMessageError, Queue.SendBatch<Resource, Msg>>;
+  ): Effect.Effect<void, SendMessageError, Queue.SendBatch<ID, Msg>>;
 };
 
 export declare namespace Queue {
-  // export interface Send<Resource extends string> extends Binding<Resource, "Queue::Send"> {}
-  export type Send<Resource extends string, Msg> = Allow<
-    Resource,
+  // export interface Send<ID extends string> extends Binding<ID, "Queue::Send"> {}
+  export type Send<ID extends string, Msg> = Allow<
+    ID,
     "Queue::Send",
     { message: Msg }
   >;
-  export function Send<Resource extends string, Msg>(
-    queue: Queue<Resource, Msg>,
-  ): Send<Resource, Msg>;
-  export type SendBatch<Resource extends string, Msg> = Allow<
-    Resource,
+  export function Send<ID extends string, Msg>(
+    queue: Queue<ID, Msg>,
+  ): Send<ID, Msg>;
+  export type SendBatch<ID extends string, Msg> = Allow<
+    ID,
     "Queue::SendBatch",
     { message: Msg[] }
   >;
-  export function SendBatch<Resource extends string, Msg>(
-    queue: Queue<Resource, Msg>,
-  ): SendBatch<Resource, Msg>;
+  export function SendBatch<ID extends string, Msg>(
+    queue: Queue<ID, Msg>,
+  ): SendBatch<ID, Msg>;
 }
 
-export function Queue<Resource extends string>(id: Resource) {
-  return <T>(): Queue<Resource, T> => ({
+export function Queue<ID extends string>(id: ID) {
+  return <T>(): Queue<ID, T> => ({
     send: (message: T) => Effect.succeed(void 0),
     sendBatch: (...message: T[]) => Effect.succeed(void 0),
   });
 }
 
-export type Bucket<Resource extends string> = {
+export type Bucket<ID extends string> = {
   get<const Key extends string = string>(
     key: Key,
-  ): Effect.Effect<string | undefined, never, Bucket.Get<Resource, Key>>;
+  ): Effect.Effect<string | undefined, never, Bucket.Get<ID, Key>>;
   put(
     key: string,
     value: string,
-  ): Effect.Effect<void, never, Bucket.Put<Resource, string>>;
+  ): Effect.Effect<void, never, Bucket.Put<ID, string>>;
 };
 
-export declare function Bucket<Resource extends string>(
-  id: Resource,
-): Bucket<Resource>;
+export declare function Bucket<ID extends string>(id: ID): Bucket<ID>;
 
 export declare namespace Bucket {
-  export type Get<Resource extends string, Key extends string> = Allow<
-    Resource,
+  export type Get<ID extends string, Key extends string> = Allow<
+    ID,
     "Bucket::Get",
     { key: Key }
   >;
-  export function Get<Resource extends string, const Key extends string>(
-    bucket: Bucket<Resource>,
+  export function Get<ID extends string, const Key extends string>(
+    bucket: Bucket<ID>,
     key?: Key,
-  ): Get<Resource, Key>;
-  export type Put<Resource extends string, Key extends string> = Allow<
-    Resource,
+  ): Get<ID, Key>;
+  export type Put<ID extends string, Key extends string> = Allow<
+    ID,
     "Bucket::Put",
     { key: Key }
   >;
-  export function Put<Resource extends string, Key extends string>(
-    bucket: Bucket<Resource>,
-  ): Put<Resource, Key>;
+  export function Put<ID extends string, Key extends string>(
+    bucket: Bucket<ID>,
+  ): Put<ID, Key>;
 }
 
 export type KVNamespace<
-  Resource extends string,
+  ID extends string,
   Key extends string,
   Value extends string,
 > = {
   get(
     key: Key,
-  ): Effect.Effect<
-    Value | undefined,
-    never,
-    KVNamespace.Get<Resource, Key, Value>
-  >;
+  ): Effect.Effect<Value | undefined, never, KVNamespace.Get<ID, Key, Value>>;
   put(
     key: Key,
     value: Value,
-  ): Effect.Effect<void, never, KVNamespace.Put<Resource, Key, Value>>;
+  ): Effect.Effect<void, never, KVNamespace.Put<ID, Key, Value>>;
 };
-export function KVNamespace<Resource extends string>(id: Resource) {
+export function KVNamespace<ID extends string>(id: ID) {
   return <
     Key extends string = string,
     Value extends string = string,
-  >(): KVNamespace<Resource, Key, Value> => ({
+  >(): KVNamespace<ID, Key, Value> => ({
     get: (key: Key) => Effect.succeed(undefined),
     put: (key: Key, value: Value) => Effect.succeed(void 0),
   });
 }
 export declare namespace KVNamespace {
   export type Get<
-    Resource extends string,
+    ID extends string,
     Key extends string,
     Value extends string,
-  > = Allow<Resource, "KV::Get", { key: Key; value: Value }>;
+  > = Allow<ID, "KV::Get", { key: Key; value: Value }>;
   export function Get<
-    Resource extends string,
+    ID extends string,
     Key extends string,
     Value extends string,
-  >(kv: KVNamespace<Resource, Key, Value>): Get<Resource, Key, Value>;
+  >(kv: KVNamespace<ID, Key, Value>): Get<ID, Key, Value>;
   export type Put<
-    Resource extends string,
+    ID extends string,
     Key extends string,
     Value extends string,
-  > = Allow<Resource, "KV::Put", { key: Key; value: Value }>;
+  > = Allow<ID, "KV::Put", { key: Key; value: Value }>;
   export function Put<
-    Resource extends string,
+    ID extends string,
     Key extends string,
     Value extends string,
-  >(kv: KVNamespace<Resource, Key, Value>): Put<Resource, Key, Value>;
+  >(kv: KVNamespace<ID, Key, Value>): Put<ID, Key, Value>;
 }
 
 const queue = Queue("queue")<string>();
 
 const kv = KVNamespace("kv")();
 
-type MinimalPolicy<E> = E extends Effect.Effect<any, any, infer S>
-  ? Policy<Extract<S, Statement>>
-  : never;
+type GetStatements<E> = E extends Effect.Effect<any, any, infer S>
+  ? Extract<S, Statement>
+  : Extract<E, Statement>;
+
+type MinimalPolicy<E> = Policy<GetStatements<E>>;
 
 declare namespace alchemy {
   function bind<S extends readonly Statement<any>[]>(
     ...actions: S
   ): Policy<S[number]>;
 
-  function deploy<E extends Effect.Effect<any, any, any>>(
-    effect: E,
-    policy: NoInfer<MinimalPolicy<E>>,
+  function deploy<W extends Worker<string, any, any>>(
+    worker: W,
+    policy: NoInfer<MinimalPolicy<W["requirements"]>>,
   ): Effect.Effect<void, never, never>;
+
+  function client<W extends Worker<string, any, any>>(
+    baseUrl: string,
+  ): {
+    fetch(
+      url: string,
+      init?: RequestInit,
+    ): Effect.Effect<Response, W["error"], never>;
+    fetch(request: Request): Effect.Effect<Response, W["error"], never>;
+  };
 }
 
 export interface Allow<
-  Resource extends string,
+  ID extends string,
   Action extends string,
   Condition = any,
 > {
   effect: "Allow";
   action: Action;
-  resource: Resource;
+  resource: ID;
   condition?: Condition;
 }
 
 export interface Deny<
-  Resource extends string,
+  ID extends string,
   Action extends string,
   Condition = any,
 > {
   effect: "Deny";
   action: Action;
-  resource: Resource;
+  resource: ID;
   condition?: Condition;
 }
 
 export type Statement<
-  Resource extends string = string,
+  ID extends string = string,
   Action extends string = string,
-> = Allow<Resource, Action> | Deny<Resource, Action>;
+> = Allow<ID, Action> | Deny<ID, Action>;
 
 // A policy is invariant over its allowed actions
 export interface Policy<in out Statements extends Statement> {
   readonly statements: Statements;
 }
 
+export type Worker<ID extends string = string, Err = never, Req = any> = {
+  resource: ID;
+  /** @internal */
+  requirements: Req;
+  /** @internal  */
+  error: Err;
+  fetch: (request: Request) => Effect.Effect<Response, Err, Worker.Fetch<ID>>;
+};
+
+export declare function Worker<ID extends string, Err = never, Req = any>(
+  id: ID,
+  props: {
+    fetch: (request: Request) => Effect.Effect<Response, Err, Req>;
+  },
+): Worker<ID, Err, Req>;
+
+export declare namespace Worker {
+  export type Fetch<ID extends string> = Allow<ID, "Worker::Fetch">;
+  export function Fetch<ID extends string>(worker: Worker<ID>): Fetch<ID>;
+}
+
+// src/backend.ts
 const bucket = Bucket("bucket");
 
-const fetch = Effect.gen(function* () {
-  return yield* bucket.get("key");
+const backend = Worker("backend", {
+  fetch: (request: Request) =>
+    Effect.gen(function* () {
+      return new Response(yield* bucket.get(request.url));
+    }),
 });
 
-const deployment = alchemy.deploy(
-  fetch,
-  alchemy.bind(Bucket.Get(bucket, "key")),
+// src/frontent.tsx
+const client = alchemy.client<typeof backend>("https://api.example.com");
+
+Effect.gen(function* () {
+  yield* client.fetch("/api/data", {
+    method: "GET",
+  });
+});
+
+const frontend = Worker("frontend", {
+  fetch: (request: Request) =>
+    Effect.gen(function* () {
+      if (request.url.startsWith("/api/")) {
+        return yield* Effect.fail(new Error("Not implemented"));
+      }
+      return yield* backend.fetch(request);
+    }),
+});
+
+const deployBackend = alchemy.deploy(backend, alchemy.bind(Bucket.Get(bucket)));
+
+const deployFrontend = alchemy.deploy(
+  frontend,
+  alchemy.bind(Worker.Fetch(backend)),
 );
 
-await Effect.runPromise(deployment);
+await Effect.runPromise(deployBackend);
+await Effect.runPromise(deployFrontend);
