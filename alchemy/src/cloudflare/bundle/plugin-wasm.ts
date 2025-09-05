ce2402630225a6de3189575d3075f30d4e7d145a
John Royal <34844819+john-royal@users.noreply.github.com>
2025-09-05 12:58:47 -0400

    fix(cloudflare): handle "?module" suffix in wasm imports

--- alchemy/src/cloudflare/bundle/plugin-wasm.ts
+++ alchemy/src/cloudflare/bundle/plugin-wasm.ts
@@ -12,21 +12,31 @@ export function createWasmPlugin() {
       build.onStart(() => {
         modules.clear();
       });
-      build.onResolve({ filter: /\.wasm$/ }, async (args) => {
-        if (!modules.has(args.path)) {
-          const name = path.normalize(args.path);
-          assert(build.initialOptions.outdir, "outdir is required");
-          await fs.mkdir(build.initialOptions.outdir, { recursive: true });
-          await fs.copyFile(
-            path.join(args.resolveDir, name),
-            path.join(build.initialOptions.outdir, name),
-          );
-          modules.set(args.path, {
-            type: "wasm",
-            path: name,
-          });
+      // Handle imports like `import "./foo.wasm"` and `import "./foo.wasm?module"`
+      // TODO(john): Figure out why this suddenly became necessary
+      build.onResolve({ filter: /\.wasm(\?module)?$/ }, async (args) => {
+        const resolved = modules.get(args.path);
+        if (resolved) {
+          return { external: true, path: resolved.path };
         }
-        return { external: true };
+
+        // Normalize path and remove the `?module` query param so we have the actual file name to copy
+        const name = path.normalize(args.path).replace(/\?module$/, "");
+
+        // Copy to outdir so it's included in the upload
+        assert(build.initialOptions.outdir, "outdir is required");
+        await fs.mkdir(build.initialOptions.outdir, { recursive: true });
+        await fs.copyFile(
+          path.join(args.resolveDir, name),
+          path.join(build.initialOptions.outdir, name),
+        );
+        modules.set(args.path, {
+          type: "wasm",
+          path: name,
+        });
+
+        // Resolve to the normalized file name (the `?module` query param is not needed in workerd)
+        return { external: true, path: name };
       });
     },
   };
