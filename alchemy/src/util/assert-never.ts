de081ce2b50df78faccf720e2e0bed32beec005d
Sam Goodwin <sam@alchemy.run>
2025-09-09 17:37:04 -0700

    fix assert never

--- alchemy/src/util/assert-never.ts
+++ alchemy/src/util/assert-never.ts
@@ -1,5 +1,3 @@
-import { inspect } from "node:util";
-
 export function assertNever(value: never, message?: string): never {
-  throw new Error(message ?? `Unexpected value: ${inspect(value)}`);
+  throw new Error(message ?? `Unexpected value: ${JSON.stringify(value)}`);
 }
