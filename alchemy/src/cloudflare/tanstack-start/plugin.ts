c72578d7a8ee39bb5047d26ab50ee142097e2f78
John Royal <34844819+john-royal@users.noreply.github.com>
2025-09-29 13:59:58 -0400

    format and lint

--- alchemy/src/cloudflare/tanstack-start/plugin.ts
+++ alchemy/src/cloudflare/tanstack-start/plugin.ts
@@ -1,13 +1,6 @@
 import type { PluginConfig } from "@cloudflare/vite-plugin";
 import alchemyVite from "../vite/plugin.ts";
 
-/**
- * TanStackStart server functions and middleware run in Node.js intead of Miniflare when using `vite dev`.
- *
- * This plugin polyfills the cloudflare:workers module & includes `process.env` during the dev server phase.
- *
- * @see https://developers.cloudflare.com/workers/framework-guides/web-apps/tanstack/#using-cloudflare-bindings
- */
 export default function alchemy(options: PluginConfig = {}) {
   return alchemyVite({
     viteEnvironment: { name: "ssr" },
