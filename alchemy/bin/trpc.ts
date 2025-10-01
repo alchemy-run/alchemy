c774351312867f68a8b66b3785920b1e6359c485
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-10-01 14:38:43 -0400

    Update trpc.ts

--- alchemy/bin/trpc.ts
+++ alchemy/bin/trpc.ts
@@ -1,7 +1,7 @@
 import { cancel, log } from "@clack/prompts";
 import pc from "picocolors";
 import { trpcServer, type TrpcCliMeta } from "trpc-cli";
-import { createAndSendEvent } from "../src/util/telemetry/events.ts";
+import { createAndSendEvent } from "../src/util/telemetry.ts";
 
 export const t = trpcServer.initTRPC.meta<TrpcCliMeta>().create();
 
