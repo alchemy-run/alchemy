bb3a33b1ea670279f35d8533339765245f5f66fe
John Royal <34844819+john-royal@users.noreply.github.com>
2026-04-03 16:01:18 -0400

    remove esbuild, rspack, and other unused bundling dependencies

--- alchemy-effect/src/Bundle/index.ts
+++ alchemy-effect/src/Bundle/index.ts
@@ -1,6 +1,4 @@
 export * from "./Bundle.ts";
 export * from "./Bundler.ts";
 export * from "./Docker.ts";
-export * from "./ESBuild.ts";
 export * from "./Rolldown.ts";
-export * from "./Rspack.ts";
