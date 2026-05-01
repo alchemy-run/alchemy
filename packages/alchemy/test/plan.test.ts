fd383711de8fef43a15e2f3fea6b3207309fed37
Sam Goodwin <sam@alchemy.run>
2026-05-01 15:29:21 -0700

    only run update out of order when there are cycles

--- packages/alchemy/test/plan.test.ts
+++ packages/alchemy/test/plan.test.ts
@@ -2189,6 +2189,7 @@ describe("unsatisfied cycle detection", () => {
       expect(Exit.isSuccess(exit)).toBe(true);
     }),
   );
+
 });
 
 describe("unresolved plan inputs in diff should conservatively update", () => {
