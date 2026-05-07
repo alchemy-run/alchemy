8343c531a3169864eea168fa086892b9a827b02f
John Royal <34844819+john-royal@users.noreply.github.com>
2026-05-06 22:04:31 -0400

    fix(cli): correct args for dev command in node

--- packages/alchemy/src/Cli/commands/dev.ts
+++ packages/alchemy/src/Cli/commands/dev.ts
@@ -32,14 +32,13 @@ export const devCommand = Command.make(
       dev: true,
     });
     const bin = typeof globalThis.Bun !== "undefined" ? "bun" : "node";
+    const main = fileURLToPath(import.meta.resolve(execEntry));
     const child = yield* ChildProcess.make(
       bin,
-      [
-        "run",
-        "--watch",
-        "--no-clear-screen",
-        fileURLToPath(import.meta.resolve(execEntry)),
-      ],
+      {
+        bun: ["run", "--watch", "--no-clear-screen", main],
+        node: ["--watch", main],
+      }[bin],
       {
         stdin: "inherit",
         stdout: "inherit",
