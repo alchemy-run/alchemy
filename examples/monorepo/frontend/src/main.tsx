327a5ae7e7ec0b1d744dc9361300c71ea42bec65
Sam Goodwin <sam@alchemy.run>
2026-05-10 01:17:00 -0700

    export ./Client

--- examples/monorepo/frontend/src/main.tsx
+++ examples/monorepo/frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { BackendClient } from "backend";
+import { BackendClient } from "backend/Client";
 import * as Effect from "effect/Effect";
 import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 import React from "react";
