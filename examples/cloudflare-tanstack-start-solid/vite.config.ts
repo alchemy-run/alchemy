bab0b4053299934ac36e7c9cd0abd81b861037a1
Michael (Pear) <michael@alchemy.run>
2026-05-20 06:00:35 -0400

    fix: remove port in example

--- examples/cloudflare-tanstack-start-solid/vite.config.ts
+++ examples/cloudflare-tanstack-start-solid/vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 import viteSolid from "vite-plugin-solid";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   resolve: {
     tsconfigPaths: true,
   },
