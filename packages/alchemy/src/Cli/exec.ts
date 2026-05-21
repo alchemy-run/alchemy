233c5dd222ef018e136bac05424c29702077bcd2
John Royal <34844819+john-royal@users.noreply.github.com>
2026-05-20 20:40:27 -0400

    correct import

--- packages/alchemy/src/Cli/exec.ts
+++ packages/alchemy/src/Cli/exec.ts
@@ -7,12 +7,12 @@ import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 import { AlchemyContextLive } from "../AlchemyContext.ts";
 import { CredentialsStoreLive } from "../Auth/Credentials.ts";
 import { ProfileLive } from "../Auth/Profile.ts";
-import * as RpcProviderClient from "../Local/RpcProviderProxy.ts";
+import * as RpcProviderProxy from "../Local/RpcProviderProxy.ts";
 import { PlatformServices } from "../Util/PlatformServices.ts";
 import { execStack, ExecStackOptions } from "./commands/deploy.ts";
 import { selectCli } from "./selectCli.ts";
 
-const services = Layer.merge(selectCli(), RpcProviderClient.fromEnv()).pipe(
+const services = Layer.merge(selectCli(), RpcProviderProxy.fromEnv()).pipe(
   Layer.provideMerge(
     Layer.mergeAll(AlchemyContextLive, ProfileLive, CredentialsStoreLive),
   ),
