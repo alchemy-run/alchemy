b5c0d171b922497e971ae20d39f45554156dab6b
Michael (Pear) <michael@alchemy.run>
2026-06-04 14:21:00 -0400

    fix claudes shit

--- examples/cloudflare-static-site/alchemy.run.ts
+++ examples/cloudflare-static-site/alchemy.run.ts
@@ -12,9 +12,10 @@ export default Alchemy.Stack(
     //fix url when no dev command present
     const worker = yield* Cloudflare.StaticSite("Website", {
       command: "zola build",
-      dev: {
-        command: "zola serve",
-      },
+      // dev: {
+      //   command: "zola serve",
+      //   url: "http://localhost:1111",
+      // },
       outdir: "public",
       assetsConfig: {
         notFoundHandling: "404-page",
