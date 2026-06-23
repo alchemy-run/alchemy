2ee86fe54c688eb8ca0a76e9e262e33915a699d2
John Royal <34844819+john-royal@users.noreply.github.com>
2026-06-23 12:34:55 -0400

    provide layer in command/local

--- packages/alchemy/src/Command/Local.ts
+++ packages/alchemy/src/Command/Local.ts
@@ -1,4 +1,6 @@
+import * as Layer from "effect/Layer";
 import * as RpcServer from "../Local/RpcServer.ts";
+import { CommandExecutorLive } from "./Command.ts";
 import { DevProviderLocal } from "./Dev.ts";
 
-DevProviderLocal().pipe(RpcServer.launch);
+DevProviderLocal().pipe(Layer.provide(CommandExecutorLive()), RpcServer.launch);
