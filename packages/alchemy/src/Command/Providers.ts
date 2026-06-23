3074ff8c1a8e42fc14a1a8237cc93e1fd45353f3
John Royal <34844819+john-royal@users.noreply.github.com>
2026-06-23 13:51:52 -0400

    add noop for old resources

--- packages/alchemy/src/Command/Providers.ts
+++ packages/alchemy/src/Command/Providers.ts
@@ -1,10 +1,27 @@
+import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
+import * as Provider from "../Provider.ts";
+import { Resource } from "../Resource.ts";
 import { BuildProvider } from "./Build.ts";
 import { CommandExecutorLive } from "./Command.ts";
 import { DevProvider } from "./Dev.ts";
 import { ExecProvider } from "./Exec.ts";
 
+const Noop = <TKind extends string>(kind: TKind) =>
+  Provider.succeed(Resource<Resource<TKind>>(kind), {
+    list: () => Effect.succeed([]),
+    diff: () => Effect.succeed({ action: "noop" }),
+    reconcile: () => Effect.succeed({}),
+    delete: () => Effect.void,
+  });
+
+const legacy = () =>
+  Layer.merge(
+    Noop("Build.Command"),
+    Noop("Build.DevServer"),
+  ) as Layer.Layer<never>;
+
 export const providers = () =>
-  Layer.mergeAll(BuildProvider(), DevProvider(), ExecProvider()).pipe(
+  Layer.mergeAll(BuildProvider(), DevProvider(), ExecProvider(), legacy()).pipe(
     Layer.provide(CommandExecutorLive()),
   );
