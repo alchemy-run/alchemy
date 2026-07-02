oddlynew/alchemy-effect

example(cloudflare-pr-package): pr-package upload/download/auth example
Rename the `cloudflare-worker-auth` example to `cloudflare-pr-package` — it
demonstrates the `@alchemy.run/pr-package` library specifically, so the name
should say so. The worker is `PrPackage.handler()` and the bearer token comes
from `PrPackage.AuthTokenValue`.

Integration tests against a real deploy:
  - bearer auth: valid -> 200, invalid / missing / "<redacted>" -> 401
  - upload a bundle and download the exact bytes back (direct + via tag)
  - garbage collection: a bundle is removed once its last tag pointer is deleted

Bumps the distilled submodule pin to include the Cloudflare error-class
tree-shaking fix (distilled#352) so the Worker bundle no longer ships a
dangling `import "@distilled.cloud/cloudflare/flagship"`. The pin is one commit
past v0.25.2 and not yet a published release; re-pin to the tagged release once
distilled cuts it.

examples/cloudflare-pr-package/src/Api.ts
@@ -1,0 +1,20 @@
+import * as PrPackage from "@alchemy.run/pr-package";
+import * as Cloudflare from "alchemy/Cloudflare";
+
+/**
+ * The real `@alchemy.run/pr-package` Worker. `PrPackage.handler` wires up the
+ * R2 bucket, KV tag index, Secrets Store bearer token, and the PackageStore
+ * Durable Object internally — this file is the Worker entry, so it must set
+ * `main: import.meta.filename`.
+ *
+ * The handler's bearer-token check is the worker side of the bug fixed in
+ * https://github.com/alchemy-run/alchemy-effect/pull/598.
+ */
+export default class Api extends Cloudflare.Worker<Api>()(
+  "Api",
+  {
+    main: import.meta.filename,
+    url: true,
+  },
+  PrPackage.handler(),
+) {}

alchemy-run/alchemy

examples/cloudflare-pr-package/src/Api.ts
@@ -1,0 +1,20 @@
+import * as PrPackage from "@alchemy.run/pr-package";
+import * as Cloudflare from "alchemy/Cloudflare";
+
+/**
+ * The real `@alchemy.run/pr-package` Worker. `PrPackage.handler` wires up the
+ * R2 bucket, KV tag index, Secrets Store bearer token, and the PackageStore
+ * Durable Object internally — this file is the Worker entry, so it must set
+ * `main: import.meta.url`.
+ *
+ * The handler's bearer-token check is the worker side of the bug fixed in
+ * https://github.com/alchemy-run/alchemy-effect/pull/598.
+ */
+export default class Api extends Cloudflare.Worker<Api>()(
+  "Api",
+  {
+    main: import.meta.url,
+    url: true,
+  },
+  PrPackage.handler(),
+) {}