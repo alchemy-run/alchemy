4c7cc6b47667318b6ee792f00ea62cc0dce057dc
Adaptura <pokriefkedev@gmail.com>
2026-09-14 20:36:42 +0200

    Consolidate SecurityGroup egress coverage into live provider test

--- packages/alchemy/test/AWS/EC2/SecurityGroup.test.ts
+++ packages/alchemy/test/AWS/EC2/SecurityGroup.test.ts
@@ -2,6 +2,7 @@ import * as AWS from "@/AWS";
 import { SecurityGroup, Vpc } from "@/AWS/EC2";
 import * as Provider from "@/Provider";
 import * as Test from "./VpcTest.ts";
+import * as EC2 from "@distilled.cloud/aws/ec2";
 import { expect } from "alchemy-test";
 import * as Effect from "effect/Effect";
 import { MinimumLogLevel } from "effect/References";
@@ -40,4 +41,53 @@ test.provider("list enumerates the deployed Security Group", (stack) =>
     yield* assertSecurityGroupGone(sg.groupId);
     yield* assertVpcGone(vpc.vpcId);
   }).pipe(logLevel),
+);
+
+const securityGroupStack = (egress?: []) =>
+  Effect.gen(function* () {
+    const vpc = yield* Vpc("EmptyEgressVpc", {
+      cidrBlock: "10.0.0.0/16",
+    });
+    const sg = yield* SecurityGroup("EmptyEgressSg", {
+      vpcId: vpc.vpcId,
+      ...(egress === undefined ? {} : { egress }),
+    });
+    return { sg, vpc };
+  });
+
+const describeEgress = (groupId: string) =>
+  EC2.describeSecurityGroupRules({
+    Filters: [{ Name: "group-id", Values: [groupId] }],
+  }).pipe(
+    Effect.map((result) =>
+      (result.SecurityGroupRules ?? []).filter((rule) => rule.IsEgress),
+    ),
+  );
+
+test.provider(
+  "distinguishes explicit empty egress from omitted egress",
+  (stack) =>
+    Effect.gen(function* () {
+      yield* stack.destroy();
+
+      const initial = yield* stack.deploy(securityGroupStack([]));
+      expect(yield* describeEgress(initial.sg.groupId)).toEqual([]);
+
+      const unchanged = yield* stack.deploy(securityGroupStack([]));
+      expect(yield* describeEgress(unchanged.sg.groupId)).toEqual([]);
+
+      const defaulted = yield* stack.deploy(securityGroupStack());
+      const defaultEgress = yield* describeEgress(defaulted.sg.groupId);
+      expect(defaultEgress).toHaveLength(1);
+      expect(defaultEgress[0]?.IpProtocol).toEqual("-1");
+      expect(defaultEgress[0]?.CidrIpv4).toEqual("0.0.0.0/0");
+
+      const restored = yield* stack.deploy(securityGroupStack([]));
+      expect(yield* describeEgress(restored.sg.groupId)).toEqual([]);
+
+      yield* stack.destroy();
+      yield* assertSecurityGroupGone(restored.sg.groupId);
+      yield* assertVpcGone(restored.vpc.vpcId);
+    }).pipe(logLevel),
+  { timeout: 120_000 },
 );
